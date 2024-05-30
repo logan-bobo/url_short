78830118f8fdd50aa573d3151cb73a8bb76ddaef
Logan Cox <mail@logan-cox.com>
2024-05-30 08:21:21 +0100

    test: add unit test for internal shortener package

--- main.go
+++ main.go
@@ -4,10 +4,21 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"database/sql"
+	"url-short/internal/database"
 )
 
 func main() {
 	serverPort := os.Getenv("SERVER_PORT")
+	dbURL := os.Getenv("PG_CONN")
+
+	db, err := sql.Open("postgres", dbURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries := database.New(db)
 
 	mux := http.NewServeMux()
 
@@ -16,9 +27,12 @@ func main() {
 		Handler: mux,
 	}
 
-	apiCfg := apiConfig{}
+	apiCfg := apiConfig{
+		DB: dbQueries,
+	}
 
 	mux.HandleFunc("GET /api/v1/healthz", apiCfg.healthz)
+	mux.HandleFunc("POST /api/v1/data/shorten", apiCfg.POSTLongURL)
 
 	log.Printf("Serving port : %v \n", serverPort)
 	log.Fatal(server.ListenAndServe())
